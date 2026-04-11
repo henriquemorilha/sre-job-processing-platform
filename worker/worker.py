46a1c7ce0042e69f2aa7646fa8d1fa455bf3f880
Henrique Morilha <henriquemorilha97@gmail.com>
2026-04-11 06:05:35 -0300

    update worker to use REDIS_URL env var

--- worker/worker.py
+++ worker/worker.py
@@ -1,6 +1,7 @@
 import logging
 import time
 import redis
+import os
 from prometheus_client import Counter, start_http_server
 
 # Configuração de logging
@@ -9,8 +10,18 @@ logging.basicConfig(
     format="%(asctime)s %(levelname)s: %(message)s"
 )
 
-# Conexão com Redis
-redis_client = redis.Redis(host="sre-redis", port=6379, db=0)
+# Pega variável de ambiente
+redis_url = os.getenv("REDIS_URL")
+
+# Conexão com Redis (Upstash)
+if redis_url:
+    redis_client = redis.Redis.from_url(
+        redis_url,
+        decode_responses=True
+    )
+else:
+    logging.warning("REDIS_URL not set")
+    redis_client = None
 
 # Métricas customizadas
 jobs_created = Counter("jobs_created_total", "Total de jobs criados")
@@ -28,18 +39,22 @@ def process_job(job_id):
         time.sleep(2)
         redis_client.set(f"job:{job_id}:status", "completed")
         jobs_completed.inc()
-        logging.info(f"Job {job_id} concluído com sucesso")
+        logging.info(f"Job {job_id} concluído com sucesso ✅")
     except Exception as e:
         redis_client.set(f"job:{job_id}:status", "failed")
         jobs_failed.inc()
         logging.error(f"Falha ao processar job {job_id}: {e}")
 
 def main():
+    if not redis_client:
+        logging.error("Redis não configurado")
+        return
+
     while True:
         logging.info("Esperando job...")
         job_id = redis_client.rpop("job_queue")
         if job_id:
-            process_job(job_id.decode("utf-8"))
+            process_job(job_id)
         else:
             time.sleep(1)
 
